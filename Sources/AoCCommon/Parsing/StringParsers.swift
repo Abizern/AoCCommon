b0f5b8f338ed05f5550bb0ee35d86e6f5e94af06
Abizer Nasir <abizern@mac.com>
2024-12-12 11:12:50 +0000

    Add characterLines parser extension for string

--- Sources/AoCCommon/Parsing/StringParsers.swift
+++ Sources/AoCCommon/Parsing/StringParsers.swift
@@ -13,12 +13,46 @@ struct LinesParser: Parser {
   }
 }
 
+struct CharacterLineParser: Parser {
+  var body: some Parser<Substring, [Character]> {
+    Parse(Array.init) {
+      Prefix { $0 != "\n" }
+    }
+  }
+}
+
+struct CharacterLinesParser: Parser {
+  var body: some Parser<Substring, [[Character]]> {
+    Many {
+      CharacterLineParser()
+    } separator: {
+      "\n"
+    } terminator: {
+      End()
+    }
+  }
+}
+
 extension String {
   /// The String as an array af Strings, separated by newlines
   /// Errors cause a fatalError with the reason.
   public func lines() throws -> [String] {
     do {
       return try LinesParser().parse(self)
+    } catch {
+      fatalError("Unable to parse data \(error)")
+    }
+  }
+}
+
+extension String {
+  /// The String as an array af Characters, separated by newlines
+  ///
+  /// Useful where the input is a character grid.
+  /// Errors cause a fatalError with the reason.
+  public func characterLines() -> [[Character]] {
+    do {
+      return try CharacterLinesParser().parse(self)
     } catch {
       fatalError("Unable to parse data \(error)")
     }
--- Tests/AoCCommonTests/Parsing/StringParsersTests.swift
+++ Tests/AoCCommonTests/Parsing/StringParsersTests.swift
@@ -1,11 +1,24 @@
 import AoCCommon
 import Testing
 
-@Suite("Test String extension for parsing")
+@Suite("Test String Parsing extensions")
 struct StringParsingTests {
   @Test("String.lines()")
   func lines() throws {
     let str = "abcd123--\nwxyz"
     try #expect(str.lines() == ["abcd123--", "wxyz"])
   }
+
+@Test("String.characterLines()")
+  func characterLines() throws {
+    let str =
+      """
+      AB
+      CD
+      """
+
+    let lines = str.characterLines()
+    #expect(lines == [[Character("A"), Character("B")], [Character("C"), Character("D")]])
+  }
+
 }
